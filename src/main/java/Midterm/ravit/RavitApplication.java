f2c41cbb8e90033a8704a2c689d7907e52078474
RinRavit <128680924+RinRavit@users.noreply.github.com>
2024-03-29 16:35:17 +0700

    Mid-term

---
+++ src/main/java/Midterm/ravit/RavitApplication.java
@@ -0,0 +1,13 @@
+package Midterm.ravit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Midterm/ravit/RavitApplicationTests.java
@@ -0,0 +1,13 @@
+package Midterm.ravit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RavitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
